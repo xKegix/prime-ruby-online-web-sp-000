ab6df70fe270a2c6b6c0eb6e16257797d95ee728
Keginold Philip <keginoldp@hotmail.com>
2021-01-27 14:57:44 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+  start = 2
+  if num > 1
+    range = (start..num-1).to_a
+    range.none? do |numtest|
+      num % numtest == 0
+    end
+  else
+    false
+  end
+end
